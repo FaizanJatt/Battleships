7eab53aac1ed4def6ca5f8146617dbcadf1d558c
Faizan Jatt <xmuhammadfaizanx@gmail.com>
2022-09-09 22:24:10 +0500

    basic css

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// const main = document.querySelector(".main");
+
+
+
+// function createBoards(){
+
+
+//     let boxes = 64;
+//     boardFirst(boxes);
+
+    
+
+//     function boardFirst(boxes) {
+//         for(let i = 0;i<boxes;i++){
+//             const boxs = document.createElement("div");
+//             console.log(`${i}`);
+//             boxs.id = "box";
+
+
+
+//             main.appendChild(boxs);
+
+
+//         }
+
+        
+
+// };
+
+
+// };
+
+
+// createBoards();
+
+
+
+
+
+
+
