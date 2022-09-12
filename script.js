dc8b157b724fd1dc97bbc546df3b80ccaf5e5f0c
Faizan Jatt <xmuhammadfaizanx@gmail.com>
2022-09-12 20:24:29 +0500

    more styles

--- script.js
+++ script.js
@@ -1,42 +1,72 @@
 
-// const main = document.querySelector(".main");
+const main = document.querySelector(".main");
+const main2 = document.querySelector(".main2");
 
 
 
-// function createBoards(){
+function createBoards(){
 
 
-//     let boxes = 64;
-//     boardFirst(boxes);
+    let boxes = 64;
+    boardFirst(boxes);
+    boardSecond(boxes);
 
     
 
-//     function boardFirst(boxes) {
-//         for(let i = 0;i<boxes;i++){
-//             const boxs = document.createElement("div");
-//             console.log(`${i}`);
-//             boxs.id = "box";
+    
+
+    function boardFirst(boxes) {
+        for(let i = 0;i<boxes;i++){
+            const boxs = document.createElement("div");
+            boxs.id = "boxMain";
 
 
 
-//             main.appendChild(boxs);
+            main.appendChild(boxs);
 
 
-//         }
+        }
 
         
 
-// };
+};
+
+function boardSecond(boxes){
+
+    
+    for(let i = 0;i<boxes;i++){
+        const boxs = document.createElement("div");
+        boxs.id = "boxMain2";
 
 
-// };
+
+        main2.appendChild(boxs);
 
 
-// createBoards();
+    }
+
+    
+}
+
+};
+
+
+createBoards();
 
 
 
 
+const look = document.getElementById("boxMain");
+
+look.addEventListener("click", logIt());
+
+
+
+
+function logIt(){
+  
+
+}
 
 
 
